17bc7eca3f2baa6052e230934b94111dbb35a45e
BohdanMK <b.m.khristevich@gmail.com>
2024-04-24 15:17:39 +0300

    add url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '',
+  base: 'https://bohdanmk.github.io/shoes-shop',
   plugins: [vue()],
   resolve: {
     alias: {
